46fb7ec251d03aa11b8f829f50cec57807ce2193
faisababangida1 <femaxs64@gmail.com>
2026-02-09 03:48:20 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+{
+  "name": "nguru-backend",
+  "version": "1.0.0",
+  "description": "Backend API for Nguru",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "@google/generative-ai": "^0.1.3",
+    "express": "^4.18.2",
+    "cors": "^2.8.5",
+    "dotenv": "^16.0.3"
+  }
+}
